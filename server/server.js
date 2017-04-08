c8460e8f0467c769ef7dd8328dfb91b5d6bf8386
Jessica Fields <Jessica Fields>
2017-04-08 14:50:37 -0400

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+let app = express();
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+});
+
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`);
+});
