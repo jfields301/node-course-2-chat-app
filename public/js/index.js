f1c4a32d220cc4ad1f63efd7b535f9b42d69eae5
Jessica Fields <Jessica Fields>
2017-04-18 21:08:29 -0400

    Add Broadcast Message

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server');
 
-    socket.emit('createMessage', {
-        from: 'John',
-        text: 'Create message event'
-    });
+    // socket.emit('createMessage', {
+    //     from: 'John',
+    //     text: 'Create message event'
+    // });
 });
 
 socket.on('disconnect', function () {
